78bb6cc9355c7894caee98a9320b0b6eaf588d7d
Fabian J <johannsen.fabian@web.de>
2021-07-23 18:41:42 +0200

    Added customizable CSV Patterns. Closes #12

--- src/output/generator.py
+++ src/output/generator.py
@@ -3,16 +3,39 @@ import os
 
 from settings import get_default_value
 
-def generate_csv(item_dict: dict):
-    pattern = get_default_value("PATTERN")
-    csv_out = ""
-    for v in item_dict.values():
-        v['holder_and_type'] = "; ".join(
-            map(lambda x: f"{x[0]}: ({'+ '.join(x[1])})", v['holder_and_type'].items()))
-        line = ",".join([v.get(k, "") or "" for k in pattern.split(',')]) + "\n"
 
-        csv_out += line
-    return csv_out
+def generate_csv(item_dict: dict):
+    def fill_pattern(pattern: str, subpatterns: dict, terminals: dict):
+        out = ""
+
+        for p in pattern.split(",")[:-1]:
+            if p in subpatterns:
+                # If a subpattern has the attribute condition,
+                # it will be replaced only if condition's value matches the pattern name
+                if terminals.get(subpatterns[p].get('condition'), p) != p:
+                    out += ","*subpatterns[p]["p"].count(",")
+                else:
+                    out += fill_pattern(subpatterns[p]["p"],
+                                        subpatterns[p].get("subpatterns", {}), terminals)
+            # Replace Terminals by their corresponding values
+            elif p in terminals:
+                out += f"{terminals[p]},"
+            # Interpret other values as Literals
+            else:
+                out += f"{p},"
+
+        return out
+
+    pattern_json = get_default_value("PATTERN")
+    out = ""
+
+    for item in item_dict.values():
+        item['holder_and_type'] = " ".join(
+            map(lambda x: f"{x[0]}: ({';'.join(x[1])})", item['holder_and_type'].items()))
+        out += fill_pattern(pattern_json["p"],
+                            pattern_json["subpatterns"], item) + "\n"
+
+    return out
 
 
 def write_csv(csv_body: str, path: str = None):
